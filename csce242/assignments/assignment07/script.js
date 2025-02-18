ad7a4d617f24975caa72ae7700c6f66b905c2fb9
LucilleCS <Lucilleoshea001@gmail.com>
2025-02-18 13:45:25 -0500

    mostly done assignment 7

--- csce242/assignments/assignment07/script.js
+++ csce242/assignments/assignment07/script.js
@@ -1,36 +1,43 @@
 let clicks = 0;
+let climb = -40;
 document.getElementById("drawStairs").onclick = () => {
   const showButton = document.getElementById("climbStairs");
   const showStick = document.getElementById("left");
-  const stair = document.getElementById("stair");
-  const stairImg = document.getElementById("stairImg");
-
+  const showStair = document.getElementById("stair");
   showButton.style.display = "block";
   showStick.style.display = "block";
+  showStair.style.display="block"
 
-  drawStairs();
-
-  for(let i=0; i<10;i++) {
-    innerHTML +=
+  for(let i=0;i<10;i++) {
+    let hr = document.createElement("hr"); 
+    document.getElementById("stair").appendChild(hr);
   }
 
-}
 
-function drawStairs () {
 
 }
+
 document.getElementById("climbStairs").onclick = () => {
   const left = document.getElementById("left");
   const right = document.getElementById("right");
+  const person = document.getElementById("image");
+  person.style.position='relative';
   if(clicks % 2 === 0) {
     left.style.display="none"
     right.style.display="block"
+    person.style.top = climb + 'px';
+    climb = climb + -40;
   }
   else{
     right.style.display="none"
     left.style.display="block"
+    person.style.top = climb + 'px';
+    climb = climb + -40;
   }
   clicks++;
+
+
+
 }
 
 
